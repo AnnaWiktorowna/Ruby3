84c3701d7d6238bdb7a0c16a969a19ef3a980ed3
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2023-01-27 22:55:17 +0100

    метод capitalize

--- app5.rb
+++ app5.rb
@@ -2,7 +2,7 @@ print "how old are you ?"
 y = gets.to_i
 
 print "want to play?"
-x = gets.strip.capitalize
+x = gets.strip.capitalize #Строки Ruby используют метод capitalize, чтобы переписать все слова в строке с большой буквы. 
 
 if y >= 18 && x == "Y"
 puts "ok let's play"
